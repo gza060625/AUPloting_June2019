6b545c1505fdd97c0a90cd9dad95d7df8dc4dbd9
gza060625 <gza060625@gmail.com>
2019-06-07 10:12:01 -0700

    split

--- smallFunctions.py
+++ smallFunctions.py
@@ -1,6 +1,8 @@
 import re
+import numpy as np
 
 oneMinDataPath="/home/ebuntu3/#code/AUPloting_June2019/data/AUTUMNX_SALU_TGBO_2019_06_06_PT1M.txt"
+tenLinesData="/home/ebuntu3/#code/AUPloting_June2019/data/testData10.txt"
 
 
 def containLetter(line): 
@@ -14,25 +16,39 @@ def checkSegments(line,numSegments,delimiter=None):
 def validateLine(line,numSegments):
   return True if ((not containLetter(line)) and checkSegments(line,numSegments)) else False
     
+#def dataLine2Array(line,delimiter=None):
+  #result=line.split(delimiter)
+  #return result
+  
     
-    
-def loadData(dataPath,numSegments,methodDiction=None,delimiter=None):
+def loadData(dataPath,numSegments,delimiter=None):
   file=open(dataPath,'r')
+  resultArray=[]
   for line in file:
     if validateLine(line,numSegments):      
       line=line.split(delimiter)
+      resultArray.append(line)
+      
+  return resultArray
       
       
       
       
-#line="2019-06-06 00:00:30.663 157     11237.35  216.91    57844.11  88888.00"
+line="2019-06-06 00:00:30.663 157     11237.35  216.91    57844.11  88888.00"
 
 #print(containLetter(line))
 #print(checkSegments(line,7))
 #print(validateLine(line,7))
+#print(dataLine2Array(line,7))
+temp=loadData(tenLinesData,7)
+a=np.array(temp)
+for x in temp:
+  print(x)
+  
+  
+print(a[:,1])
 
-
-loadData(oneMinDataPath)
+#test
 
 
     
